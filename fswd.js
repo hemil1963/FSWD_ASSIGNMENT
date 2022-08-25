c36aa90911ee4478d34773e45c991d3d3703fe02
hemil1963 <97402724+hemil1963@users.noreply.github.com>
2022-08-25 12:02:00 +0530

    Add files via upload

---
+++ fswd.js
@@ -0,0 +1,9 @@
+class Student {
+   constructor(rollno,firstname,lastname) {
+     this.rollno=rollno;
+     this.firstname=firstname;
+     this.lastname=lastname;
+     }
+    get latest() {
+    return this.log[firstname+" "+lastname];
+   }}
